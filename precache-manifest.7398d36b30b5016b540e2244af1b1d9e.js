e7babf7be711313b8008ad582efbe8ecfc42eae5
RodriCosta <rodrigom.costa@outlook.com>
2019-10-01 17:19:37 -0300

    Initial Commit

---
+++ precache-manifest.7398d36b30b5016b540e2244af1b1d9e.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/Portifolio/static/media/git.0ba2aa20.png"
+  },
+  {
+    "revision": "4b426b6876b1d7dc9d0b",
+    "url": "/Portifolio/static/css/main.9c3c6665.chunk.css"
+  },
+  {
+    "revision": "644accefd07f19fac7ed",
+    "url": "/Portifolio/static/js/runtime~main.ecc9c45b.js"
+  },
+  {
+    "revision": "8743f4d099a45b1e0eb3",
+    "url": "/Portifolio/static/js/2.0c8a9e17.chunk.js"
+  },
+  {
+    "revision": "0e35efce0478ff71d64c4ee629e61c94",
+    "url": "/Portifolio/static/media/banner.0e35efce.jpg"
+  },
+  {
+    "revision": "81f8cd77b32f86d047d2d29240ad3240",
+    "url": "/Portifolio/static/media/rodrigo.81f8cd77.jpg"
+  },
+  {
+    "revision": "0617c2fcad92fc332b1baf226657af29",
+    "url": "/Portifolio/static/media/etec.0617c2fc.png"
+  },
+  {
+    "revision": "5d582e978dbebe409696be4f44bffcb2",
+    "url": "/Portifolio/static/media/image.5d582e97.png"
+  },
+  {
+    "revision": "ed4ed8055785e23307ff869434960fdb",
+    "url": "/Portifolio/static/media/Rodrigo Miguel da Costa.ed4ed805.pdf"
+  },
+  {
+    "revision": "8c857bafbac2010641e12441797d8063",
+    "url": "/Portifolio/static/media/a3.8c857baf.jpg"
+  },
+  {
+    "revision": "3bfabbd4884b9541e1107481b29b2f86",
+    "url": "/Portifolio/static/media/html.3bfabbd4.png"
+  },
+  {
+    "revision": "faae11a6b87547339411b40ac228d4d7",
+    "url": "/Portifolio/static/media/css.faae11a6.png"
+  },
+  {
+    "revision": "dff36bad692fa63eb3583a4c8983e3c1",
+    "url": "/Portifolio/static/media/js.dff36bad.png"
+  },
+  {
+    "revision": "da53dc3418ca99ce8fec3846274d9cb8",
+    "url": "/Portifolio/static/media/boot.da53dc34.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/Portifolio/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "3420e571b3d7a4a348d8fad91e3bfda4",
+    "url": "/Portifolio/static/media/jquery.3420e571.png"
+  },
+  {
+    "revision": "a91a86e077871ed158de1ce54b13f756",
+    "url": "/Portifolio/static/media/json.a91a86e0.png"
+  },
+  {
+    "revision": "d22e90855e7a4f1bdbe9388438e64fa3",
+    "url": "/Portifolio/static/media/ajax.d22e9085.png"
+  },
+  {
+    "revision": "406b655c7421ffd1ba9dc93c505c1bf4",
+    "url": "/Portifolio/static/media/dev.406b655c.png"
+  },
+  {
+    "revision": "4b426b6876b1d7dc9d0b",
+    "url": "/Portifolio/static/js/main.eb287bfd.chunk.js"
+  },
+  {
+    "revision": "a9aca2f4501163a0820d59e0236d2bc8",
+    "url": "/Portifolio/static/media/py.a9aca2f4.png"
+  },
+  {
+    "revision": "ca5b9d80af2c6ff6731807dfc8a93701",
+    "url": "/Portifolio/static/media/restapi.ca5b9d80.png"
+  },
+  {
+    "revision": "81eda716e82c506fbc256d278808a855",
+    "url": "/Portifolio/static/media/c.81eda716.png"
+  },
+  {
+    "revision": "6418f7bfd00a63bdebd4cd4cfb550c66",
+    "url": "/Portifolio/static/media/NET-infinity.6418f7bf.png"
+  },
+  {
+    "revision": "d049390fea4db1d5ad5070ad160016b7",
+    "url": "/Portifolio/static/media/LoginContratoFacil.d049390f.PNG"
+  },
+  {
+    "revision": "fe21910b6255cef2113c7469fea4d906",
+    "url": "/Portifolio/static/media/contrato facil formulario.fe21910b.PNG"
+  },
+  {
+    "revision": "950c66105912504b15302c2983fac5ee",
+    "url": "/Portifolio/static/media/srmLogin.950c6610.PNG"
+  },
+  {
+    "revision": "87aa38838bfd929b1754ae55d3898530",
+    "url": "/Portifolio/static/media/formularioSrm.87aa3883.PNG"
+  },
+  {
+    "revision": "e10c51ddf8483d75b31c0969f1e3f61f",
+    "url": "/Portifolio/static/media/FaturaLogin.e10c51dd.PNG"
+  },
+  {
+    "revision": "11e6901d65b3a7090b05d2c438405270",
+    "url": "/Portifolio/static/media/Primeiro-Site.11e6901d.PNG"
+  },
+  {
+    "revision": "3d42f51761eebb4c8e755c85f9a9f57e",
+    "url": "/Portifolio/static/media/site-novo.3d42f517.PNG"
+  },
+  {
+    "revision": "86faabfca6933c1ca669126749b06194",
+    "url": "/Portifolio/static/media/replica Spotfy.86faabfc.PNG"
+  },
+  {
+    "revision": "4d53b2ddb2750b9e0e7c64a5c17861df",
+    "url": "/Portifolio/static/media/tecblog.4d53b2dd.PNG"
+  },
+  {
+    "revision": "9941146fe0fda2ae448b4622f8f5c5de",
+    "url": "/Portifolio/static/media/game.9941146f.PNG"
+  },
+  {
+    "revision": "d4417cce8dd234a7db6821bad7858f6a",
+    "url": "/Portifolio/static/media/game dois.d4417cce.PNG"
+  },
+  {
+    "revision": "d2355e1fcd449d2b66c1771c3437f92a",
+    "url": "/Portifolio/static/media/logo_empresa.d2355e1f.PNG"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/Portifolio/static/media/github.0ba2aa20.png"
+  },
+  {
+    "revision": "968ea62882943e88bbd318ae5fa67429",
+    "url": "/Portifolio/static/media/linkedin.968ea628.png"
+  },
+  {
+    "revision": "8e57adcf4587eb187d91a93260094ae1",
+    "url": "/Portifolio/index.html"
+  }
+];
